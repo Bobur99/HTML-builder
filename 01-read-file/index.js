e318238f6dcb4bcbc47c4cae17f55d602f4d073d
Bobur <rasulev.bobur.1999@mail.ru>
2022-05-23 15:40:28 +0500

    task 01-read-file DONE!

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const path = require('path')
+
+fs.readFile(
+   path.join(__dirname,'text.txt'),
+   (err, data) => {
+        if(err) throw err
+        console.log(Buffer.from(data).toString());
+   }
+)
